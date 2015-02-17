1e177c47dabbf1efb2ad5c1da4ec33d814c59cc0
Chance Snow <chances@cat.pdx.edu>
2015-02-16 21:57:51 -0800

    Begin implementing application

--- bin/sandwich.js
+++ bin/sandwich.js
@@ -2,6 +2,26 @@
 
 'use strict';
 
-process.title = 'sandwich';
+var argv = require('minimist')(process.argv.slice(2));
+var Liftoff = require('liftoff');
 
-require('../')();
+//TODO: Design this system
+
+// set env var for ORIGINAL cwd
+// // before anything touches it
+var basedir = process.env.INIT_CWD = process.cwd();
+
+var cli = new Liftoff({
+    name: 'sandwich',
+    extensions: require('interpret').jsVariants
+});
+
+cli.launch({
+    cwd: argv.cwd,
+    configPath: argv.recipe,
+    require: argv.require
+}, run);
+
+function run(env) {
+    // TODO: Implement application
+}
